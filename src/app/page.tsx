75c8f41a59b0e345641d18110d7d6a4be90faff3
JoonPark <sargadil@gmail.com>
2023-12-22 14:43:39 +0900

    noroop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import "swiper/css/pagination";
 export default function Home() {
   SwiperCore.use([Pagination, Mousewheel, A11y]);
   const swiperRef = useRef<SwiperRef>(null);
-  const sections = ["first", "second", "third", "fourth", "fifth"];
+  const pages = usePageStore((state) => state.pages);
   const setSwiper = usePageStore((state) => state.setSwiper);
   const setPageIndex = usePageStore((state) => state.setPageIndex);
 
@@ -19,15 +19,14 @@ export default function Home() {
     <main className="w-full h-full flex-1 max-w-[1200px] sm:border overflow-hidden">
       <Swiper
         onSwiper={setSwiper}
-        onSlideChange={(swiper) => {setPageIndex(swiper.realIndex)}}
+        onSlideChange={(swiper) => {setPageIndex(swiper.activeIndex)}}
         ref={swiperRef}
         style={{ height: "100%" }}
         direction="vertical"
         slidesPerView={1}
         mousewheel={true}
-        loop={true}
         className="mySwiper">
-        {sections.map((section, index) => (
+        {pages.map((section, index) => (
           <SwiperSlide key={index}>
             <section className="w-full min-h-full p-10">
               it is {section} section.
