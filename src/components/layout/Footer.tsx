8019f1d71d95f4f3f10cd8f9a853a62a6a0c060c
joon <sargadil@gmail.com>
2023-12-25 22:45:52 +0900

    Remove pageTo, Update setPageIndex

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,6 @@ import { Button } from "@/components/buttons/Button";
 export default function Footer() {
   const pages = usePageStore((state) => state.pages);
   const pageIndex = usePageStore((state) => state.pageIndex);
-  const pageTo = usePageStore((state) => state.pageTo);
   const setPageIndex = usePageStore((state) => state.setPageIndex);
   const icons = [<UserSquare2Icon key={"0"} size={28} />, <BoxesIcon key={"1"} size={28} />, <AtomIcon key={"2"} size={28} />, <MailPlus key={"3"} size={28} />, <BookOpenTextIcon key={"4"} size={28} />];
 
@@ -18,7 +17,7 @@ export default function Footer() {
           key={index}
           className='w-full h-full flex justify-center items-center border-b-2 flex-col gap-1'
           variant={pageIndex === index ? "selectedFooter" : "defaultFooter"}
-          onClick={() => pageTo(index)}>
+          onClick={() => setPageIndex(index)}>
           {icons[index]}
           {button}
         </Button>
