2b9caae0b2a3a448e4fbba947137e3a88bc0f8b2
JoonPark <sargadil@gmail.com>
2023-12-28 17:37:46 +0900

    Feature Devided pages to page Components

---
+++ src/components/pages/ContactPage.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+  return (
+    <section className="w-full min-h-full p-10">
+      콘택트 페이지
+    </section>
+  );
+}
---
+++ src/components/pages/TechStackPage.tsx
@@ -0,0 +1,7 @@
+export default function TechStackPage() {
+  return (
+    <section className="w-full min-h-full p-10">
+      기술 스택 페이지
+    </section>
+  );
+}
