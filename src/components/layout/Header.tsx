380bccf96e8afbc98feb45ac19b5243de0092396
JoonPark <sargadil@gmail.com>
2024-01-08 17:35:35 +0900

    build error

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
   return (
     <header className={`z-50 w-full flex sm:hidden justify-center h-[10vh] px-10 shadow-md ${isDark && 'shadow-border'}`}>
       <div className="w-full max-w-7xl flex justify-between items-center">
-          <b className="text-2xl font-black">Joon's Webpage</b>
+          <b className="text-2xl font-black">{"Joon's Webpage"}</b>
         <DarkToggle />
       </div>
     </header>
