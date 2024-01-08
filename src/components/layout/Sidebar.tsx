939c0e411e40dd145b0e7f7fcf142dd4442cf70b
JoonPark <sargadil@gmail.com>
2024-01-08 17:02:51 +0900

    Changed nav

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,20 +3,21 @@ import DarkToggle from "../buttons/DarkToggle";
 import NextLogo from "../Logo/NextLogo";
 
 export default function Sidebar() {
+
   return (
-    <header className="hidden sm:flex flex-col min-w-max h-full justify-between p-5 border">
-      <div className="flex flex-col gap-10">
+    <aside className="hidden sm:flex flex-col h-full justify-between p-5 border">
+      <div className="flex flex-col">
         <div className="flex justify-center items-center flex-col gap-5">
           <b className="text-lg font-black">Joon Park</b>
         </div>
-        <Navbar />
       </div>
+        <Navbar isSidebar={true} />
       <div className="flex flex-col items-center">
         <div className=" w-24">
           <DarkToggle />
         </div>
         <NextLogo />
       </div>
-    </header>
+    </aside>
   );
 }
