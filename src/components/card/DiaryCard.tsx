0ab7d19e2e43ec7dd0b127ad52a25efc8cf940ad
joon <sargadil@gmail.com>
2024-01-13 15:29:48 +0900

    Diary dummy data

--- src/components/card/DiaryCard.tsx
+++ src/components/card/DiaryCard.tsx
@@ -1,21 +1,22 @@
 import { DiaryProps } from "@/types/Diary";
-export default function DiaryCard() {
-  // {id, title, content, date, ...props}: DiaryProps
-  // 1에서 5까지의 랜덤 인덱스 길이를 가지는 배열 만들기
-  const randomArray = Array.from(
-    { length: 5 },
-    () => Math.floor(Math.random() * 5) + 1,
-  );
+export default function DiaryCard({
+  title,
+  content,
+  date,
+  ...props
+}: DiaryProps) {
+
 
   return (
     <div className="bg-yellow-200 max-w-full h-auto p-6 relative shadow-diary">
-      <div className="diagonal-split-bg absolute top-0 left-0 bg-background w-6 h-6">
+      <div className="diagonal-split-bg absolute top-0 left-0 bg-background w-6 h-6"></div>
+      <div className="h-full flex flex-col justify-between">
+        <div>
+          <h2 className=" text-xl font-bold">{title}</h2>
+          <p>{content}</p>
+        </div>
+        <p className="w-full text-end">{date}</p>
       </div>
-      <h2 className=" text-xl font-bold">안녕하세요</h2>
-      {randomArray.map((item, index) => (
-        <p key={index}>하이요</p>
-      ))}
-      <p>24. 01. 12</p>
     </div>
   );
 }
