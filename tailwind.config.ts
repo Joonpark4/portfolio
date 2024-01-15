5868db90a6cb4978c1d25bad48669e0d14765a5a
JoonPark <sargadil@gmail.com>
2024-01-15 09:29:59 +0900

    darktheme memo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,10 @@ module.exports = {
           DEFAULT: "var(--card)",
           foreground: "var(--card-foreground)",
         },
+        memo: {
+          DEFAULT : "var(--memo)",
+          foreground: "var(--memo-foreground)",
+        }
       },
       borderRadius: {
         lg: "var(--radius)",
