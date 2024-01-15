7c9b24cdec3c6e61189fe294a222d8dc9affd123
JoonPark <sargadil@gmail.com>
2024-01-15 09:29:59 +0900

    darktheme memo

--- src/components/card/DiaryCard.tsx
+++ src/components/card/DiaryCard.tsx
@@ -8,9 +8,9 @@ export default function DiaryCard({
 }: DiaryProps) {
 
   return (
-    <div className="bg-yellow-200 max-w-full h-auto p-6 relative shadow-diary">
-      <div className="diagonal-split-bg absolute top-0 left-0 bg-background w-6 h-6"></div>
-      <div className="h-full flex flex-col justify-between">
+    <div className="bg-memo max-w-full h-auto p-6 relative shadow-diary">
+      <div className="diagonal-split-bg absolute top-0 left-0 w-6 h-6"></div>
+      <div className="h-full flex flex-col justify-between text-black">
         <div>
           <h2 className=" text-xl font-bold">{title}</h2>
           <p>{content}</p>
