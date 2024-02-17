c5e448e6806b6e71260228932304144518c64148
joon <sargadil@gmail.com>
2024-02-18 02:25:50 +0900

    Refactor DiaryCard component styling

--- src/components/card/DiaryCard.tsx
+++ src/components/card/DiaryCard.tsx
@@ -7,16 +7,16 @@ export default function DiaryCard({
   date,
   ...props
 }: DiaryProps) {
-
   return (
     <div
-      className={cn("bg-memo p-6 relative shadow-diary memo", props.className)}>
-      <div className="diagonal-split-bg absolute top-0 left-0 w-6 h-6"></div>
-      <div className={cn("h-full flex flex-col justify-between text-black")}>
-        <div className="flex flex-col h-full">
-          <h2 className="text-xl font-bold mb-4">{title}</h2>
+      className={cn("memo relative bg-memo p-6 shadow-diary", props.className)}
+    >
+      <div className="diagonal-split-bg absolute left-0 top-0 h-6 w-6"></div>
+      <div className={cn("flex h-full flex-col justify-between text-black")}>
+        <div className="flex h-full flex-col">
+          <h2 className="mb-4 text-xl font-bold">{title}</h2>
           <p className="h-full">{content}</p>
-          <p className="w-full text-end mt-2">{date}</p>
+          <p className="mt-2 w-full text-end">{date}</p>
         </div>
       </div>
     </div>
